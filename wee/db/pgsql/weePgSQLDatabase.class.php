ad8901d8a32d3d09534c8b0e445989b8a7bb57c3
essen <essen@dev-extend.eu>
2006-11-12 23:21:35 +0000

    PostgreSQL database driver: first import.

---
+++ wee/db/pgsql/weePgSQLDatabase.class.php
@@ -0,0 +1,123 @@
+<?php
+
+/*
+	Web:Extend
+	Copyright (c) 2006 Dev:Extend
+
+	This library is free software; you can redistribute it and/or
+	modify it under the terms of the GNU Lesser General Public
+	License as published by the Free Software Foundation; either
+	version 2.1 of the License, or (at your option) any later version.
+
+	This library is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+	Lesser General Public License for more details.
+
+	You should have received a copy of the GNU Lesser General Public
+	License along with this library; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+if (!defined('ALLOW_INCLUSION')) die;
+
+class weePgSQLDatabase extends weeDatabase
+{
+	private $rLink;
+	private $iNumAffectedRows;
+	private $iNumQueries;
+
+	public function __construct($aParams, $sLogin = null, $sPassword = null)
+	{
+		fire(!function_exists('pg_connect'), 'ConfigurationException');
+
+		//TODO:maybe quote & escape values...
+		$sConnection = null;
+		foreach ($aParams as $sKey => $sValue)
+			$sConnection .= $sKey . '=' . $sValue . ' ';
+
+		$this->rLink = pg_connect($sConnection, PGSQL_CONNECT_FORCE_NEW);
+		fire($this->rLink === false, 'DatabaseException');
+
+		// Set encoding
+
+		pg_set_client_encoding($this->rLink, 'UNICODE');
+
+		// Initialize additional database services
+
+		$this->iNumQueries = 0;
+
+		$sPath = dirname(__FILE__);
+		require_once($sPath . '/../weeDatabaseCriteria' . CLASS_EXT);
+		require_once($sPath . '/../weeDatabaseQuery' . CLASS_EXT);
+
+		//TODO:change the criteria class
+		weeDatabaseQuery::$criteriaClass	= 'weeMySQLCriteria';
+		weeDatabaseQuery::$queryClass		= 'weeDatabaseQuery';
+	}
+
+	public function __destruct()
+	{
+		pg_close($this->rLink);
+	}
+
+	// Note:	Escape string BUT it doesn't escape %
+	//			queries might be vulnerable if used with sprintf
+	//TODO:don't use sprintf with queries!!
+	//TODO:pg_escape_bytea?
+
+	public function escape($mValue)
+	{
+		return "'" . pg_escape_string($mValue) . "'";
+	}
+
+	public function getLastError()
+	{
+		return pg_last_error($this->rLink);
+	}
+
+	public function getPKId($sName = null)
+	{
+		fire(empty($sName), 'InvalidParameterException');
+		fire($this->rLink === false, 'IllegalStateException');
+
+		$r = pg_query($this->rLink, 'SELECT currval(' . $this->escape($sName) . ')');
+		fire($r === false, 'DatabaseException');
+
+		return (int)pg_fetch_result($r, 0, 0);
+	}
+
+	public function numAffectedRows()
+	{
+		fire($this->rLink === false, 'IllegalStateException');
+		return $this->iNumAffectedRows;
+	}
+
+	public function numQueries()
+	{
+		return $this->iNumQueries;
+	}
+
+	public function query($mQueryString)
+	{
+		fire($this->rLink === false, 'IllegalStateException');
+
+		$this->iNumQueries++;
+
+		if (func_num_args() > 1)
+			$mQueryString = $this->buildSafeQuery(func_get_args());
+		elseif (is_object($mQueryString))
+			$mQueryString = $mQueryString->build($this);
+
+		$rResult = pg_query($this->rLink, $mQueryString);
+		fire($rResult === false, 'DatabaseException');
+
+		// Get it now since it can be wrong if numAffectedRows is called after getPKId
+		$this->iNumAffectedRows = pg_affected_rows($rResult);
+
+		if (pg_num_rows($rResult))
+			return new weePgSQLResult($rResult);
+	}
+}
+
+?>
---
+++ wee/db/pgsql/weePgSQLResult.class.php
@@ -0,0 +1,95 @@
+<?php
+
+/*
+	Web:Extend
+	Copyright (c) 2006 Dev:Extend
+
+	This library is free software; you can redistribute it and/or
+	modify it under the terms of the GNU Lesser General Public
+	License as published by the Free Software Foundation; either
+	version 2.1 of the License, or (at your option) any later version.
+
+	This library is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+	Lesser General Public License for more details.
+
+	You should have received a copy of the GNU Lesser General Public
+	License along with this library; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+if (!defined('ALLOW_INCLUSION')) die;
+
+class weePgSQLResult extends weeDatabaseResult
+{
+	private $rResult;
+
+	public function __construct($rResult)
+	{
+		fire(!is_resource($rResult), 'InvalidArgumentException');
+		$this->rResult = $rResult;
+	}
+
+	public function __destruct()
+	{
+		pg_free_result($this->rResult);
+	}
+
+	public function fetch()
+	{
+		$a = pg_fetch_assoc($this->rResult);
+		fire($a === false, 'DatabaseException');
+
+		if ($this->bEncodeResults)
+			return weeOutput::encodeArray($a);
+		return $a;
+	}
+
+	public function fetchAll()
+	{
+		return pg_fetch_all($this->rResult);
+	}
+
+	public function numResults()
+	{
+		$i = pg_num_rows($this->rResult);
+		fire($i == -1, 'DatabaseException');
+
+		return $i;
+	}
+
+	// SPL: Iterator
+
+	private $aCurrentFetch;
+	private $iCurrentIndex;
+
+	public function current()
+	{
+		if ($this->bEncodeResults)	return weeOutput::encodeArray($this->aCurrentFetch);
+		else						return $this->aCurrentFetch;
+	}
+
+	public function key()
+	{
+		return $this->iCurrentIndex;
+	}
+
+	public function next()
+	{
+		$this->iCurrentIndex++;
+	}
+
+	public function rewind()
+	{
+		$this->iCurrentIndex = 0;
+	}
+
+	public function valid()
+	{
+		$this->aCurrentFetch = @pg_fetch_assoc($this->rResult, $this->iCurrentIndex);
+		return ($this->aCurrentFetch !== false);
+	}
+}
+
+?>
