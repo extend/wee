d90536a5f7c51b062f2ae802d5324ed940ec8bfb
essen <essen@dev-extend.eu>
2006-11-21 21:15:47 +0000

    Added new error codes from PHP 5.20. Now if an unknown error code is encountered, the code will not crash.

--- wee/weeException.class.php
+++ wee/weeException.class.php
@@ -67,9 +67,22 @@ final class weeException extends Namespace
 	{
 		if (error_reporting() == 0) return;
 
-		$aTypes = array(1 => 'E_ERROR', 2 => 'E_WARNING', 4 => 'E_PARSE', 8 => 'E_NOTICE', 16 => 'E_CORE_ERROR',
-			32 => 'E_CORE_WARNING', 64 => 'E_COMPILE_ERROR', 128 => 'E_COMPILE_WARNING', 256 => 'E_USER_ERROR',
-			512 => 'E_USER_WARNING', 1024 => 'E_USER_NOTICE', 2047 => 'E_ALL', 2048 => 'E_STRICT');
+		$aTypes = array(
+			1 => 'E_ERROR',
+			2 => 'E_WARNING',
+			4 => 'E_PARSE',
+			8 => 'E_NOTICE',
+			16 => 'E_CORE_ERROR',
+			32 => 'E_CORE_WARNING',
+			64 => 'E_COMPILE_ERROR',
+			128 => 'E_COMPILE_WARNING',
+			256 => 'E_USER_ERROR',
+			512 => 'E_USER_WARNING',
+			1024 => 'E_USER_NOTICE',
+			2048 => 'E_STRICT',
+			4096 => 'E_RECOVERABLE_ERROR',
+			8191 => 'E_ALL',
+		);
 
 		$sDebug = null;
 
@@ -82,7 +95,7 @@ final class weeException extends Namespace
 		}
 
 		self::printErrorPage($sDebug);
-		self::logError('error', $sFile, $iLine, $aTypes[$iNumber], $sMessage);
+		self::logError('error', $sFile, $iLine, (empty($aTypes[$iNumber])) ? $aTypes[1] : $aTypes[$iNumber], $sMessage);
 		exit($iNumber);
 	}
 
