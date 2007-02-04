df06f8385f55944e03118dc4b67eba57be18271e
essen <essen@dev-extend.eu>
2007-02-04 23:25:57 +0000

    Separated the two classes in two files. Added docComments, and done some maintenance.

---
+++ wee/files/weeUploadedFile.class.php
@@ -0,0 +1,179 @@
+<?php
+
+/*
+	Dev:Extend Web Library
+	Copyright (c) 2006 Dev:Extend
+
+	This software is licensed under the Dev:Extend Public License. You can use,
+	copy, modify and/or distribute the software under the terms of this License.
+
+	This software is distributed WITHOUT ANY WARRANTIES, including, but not
+	limited to, the implied warranties of MERCHANTABILITY and FITNESS FOR A
+	PARTICULAR PURPOSE. See the Dev:Extend Public License for more details.
+
+	You should have received a copy of the Dev:Extend Public License along with
+	this software; if not, you can download it at the following url:
+	http://dev-extend.eu/license/.
+*/
+
+if (!defined('ALLOW_INCLUSION')) die;
+
+/**
+	Handle files uploaded using forms.
+*/
+
+class weeUploadedFile
+{
+	/**
+		Original filename, before it was uploaded.
+	*/
+
+	public $sSrcName;
+
+	/**
+		Temporary filename, after it was uploaded but before being moved.
+	*/
+
+	public $sTmpName;
+
+	/**
+		MIME type, if available.
+		Don't rely on it for determining the MIME type of the file.
+	*/
+
+	public $sMimeType;
+
+	/**
+		Size of the file, in bytes, as reported by PHP.
+	*/
+
+	public $iSize;
+
+	/**
+		The error code specified by PHP for this file.
+
+		@see http://fr.php.net/manual/en/features.file-upload.errors.php
+	*/
+
+	public $iErrorCode;
+
+	/**
+		Initialize the file class.
+
+		@param	$sSrcName	The original name of the file on the client machine.
+		@param	$sTmpName	The temporary filename of the file in which the uploaded file was stored on the server.
+		@param	$sMimeType	The mime type of the file, if available.
+		@param	$iSize		The size, in bytes, of the uploaded file.
+		@param	$iErrorCode	The error code associated with this file upload.
+	*/
+
+	public function __construct($sSrcName, $sTmpName, $sMimeType, $iSize, $iErrorCode)
+	{
+		$this->sSrcName		= $sSrcName;
+		$this->sTmpName		= $sTmpName;
+		$this->sMimeType	= $sMimeType;
+		$this->iSize		= $iSize;
+		$this->iErrorCode	= $iErrorCode;
+	}
+
+	/**
+		Tests if given destination file exists.
+		Use it to check before moving a file if you won't ecrase another.
+
+		@param	$sDestination	The path of the destination file.
+		@param	$sNewFilename	The destination filename. If null, it is the same filename as the source file.
+		@return	bool			True if the file exists, false otherwise.
+	*/
+
+	public function fileExists($sDestination, $sNewFilename = null)
+	{
+		if (empty($sNewFilename))
+			$sNewFilename	= $this->getFilename();
+
+		return file_exists($sDestination . '/' . $sNewFilename);
+	}
+
+	/**
+		Returns the error string for this file error.
+		Do not call it if the file is good (isOK returns true).
+
+		@return string The error message for this file error code.
+	*/
+
+	public function getError()
+	{
+		fire($this->iErrorCode == UPLOAD_ERR_OK, 'IllegalStateException');
+
+		$aErrorMessages = array(
+			UPLOAD_ERR_INI_SIZE		=> 'The uploaded file size exceeds the upload_max_filesize directive in php.ini.',
+			UPLOAD_ERR_FORM_SIZE	=> 'The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form.',
+			UPLOAD_ERR_PARTIAL		=> 'The uploaded file was only partially uploaded.',
+			UPLOAD_ERR_NO_FILE		=> 'No file was uploaded.',
+			UPLOAD_ERR_NO_TMP_DIR	=> 'Missing a temporary folder.',
+			UPLOAD_ERR_CANT_WRITE	=> 'Failed to write file to disk.',
+		);
+
+		if (!empty($aErrorMessages[$this->iErrorCode]))
+			return _($aErrorMessages[$this->iErrorCode]);
+
+		return _('Unknown error.');
+	}
+
+	/**
+		Returns the extension of the file.
+
+		@return string The file's extension.
+	*/
+
+	public function getExt()
+	{
+		$m	= strrchr($this->getFilename(), '.');
+		if ($m === false)
+			return null;
+		return substr($m, 1);
+	}
+
+	/**
+		Returns the filename without including the path.
+
+		@return string The file's filename.
+	*/
+
+	public function getFilename()
+	{
+		return basename($this->sSrcName);
+	}
+
+	/**
+		Tests if the file is correctly uploaded.
+
+		@return bool True if the file is OK, false otherwise.
+	*/
+
+	public function isOK()
+	{
+		return $this->iErrorCode == UPLOAD_ERR_OK;
+	}
+
+	/**
+		Moves the file to the specified folder, and rename it if needed.
+
+		@param	$sDestination	The path of the destination file.
+		@param	$sNewFilename	The destination filename. If null, it is the same filename as the source file.
+		@return	bool			True if the file was successfully moved, false otherwise.
+	*/
+
+	public function moveTo($sDestination, $sNewFilename = null)
+	{
+		clearstatcache();
+		fire(!is_dir($sDestination), 'InvalidArgumentException');
+		fire(!is_uploaded_file($this->sTmpName), 'UnexpectedValueException');
+
+		if (empty($sNewFilename))
+			$sNewFilename = $this->getFilename();
+
+		return move_uploaded_file($this->sTmpName, $sDestination . '/' . $sNewFilename);
+	}
+}
+
+?>
--- wee/files/weeUploadedFiles.class.php
+++ wee/files/weeUploadedFiles.class.php
@@ -18,109 +18,62 @@
 
 if (!defined('ALLOW_INCLUSION')) die;
 
-if (!defined('UPLOAD_ERR_NO_TMP_DIR'))	define('UPLOAD_ERR_NO_TMP_DIR',	6);
+if (!defined('UPLOAD_ERR_NO_TMP_DIR'))	define('UPLOAD_ERR_NO_TMP_DIR', 6);
 if (!defined('UPLOAD_ERR_CANT_WRITE'))	define('UPLOAD_ERR_CANT_WRITE', 7);
 
-class weeUploadedFile
-{
-	public $sSrcName;
-	public $sTmpName;
-	public $sMimeType;
-	public $iSize;
-	public $iErrorCode;
-
-	public function __construct($sSrcName, $sTmpName, $sMimeType, $iSize, $iErrorCode)
-	{
-		$this->sSrcName		= $sSrcName;
-		$this->sTmpName		= $sTmpName;
-		$this->sMimeType	= $sMimeType;
-		$this->iSize		= $iSize;
-		$this->iErrorCode	= $iErrorCode;
-	}
-
-	public function fileExists($sDestination, $sNewFilename = null)
-	{
-		if (empty($sNewFilename))
-			$sNewFilename	= $this->getFilename();
-
-		return is_file($sDestination . '/' . $sNewFilename);
-	}
-
-	//TODO:user friendly errors
-	public function getError()
-	{
-		switch ($this->iErrorCode)
-		{
-			case UPLOAD_ERR_OK:
-				Burn('IllegalStateException');
-
-			case UPLOAD_ERR_INI_SIZE:
-				return _('The uploaded file size exceeds the upload_max_filesize directive in php.ini.');
-
-			case UPLOAD_ERR_FORM_SIZE:
-				return _('The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form.');
-
-			case UPLOAD_ERR_PARTIAL:
-				return _('The uploaded file was only partially uploaded.');
-
-			case UPLOAD_ERR_NO_FILE:
-				return _('No file was uploaded.');
-
-			case UPLOAD_ERR_NO_TMP_DIR:
-				return _('Missing a temporary folder.');
-
-			case UPLOAD_ERR_CANT_WRITE:
-				return _('Failed to write file to disk.');
-
-			default:
-				return _('Unknown error.');
-		}
-	}
-
-
-	public function getExt()
-	{
-		$m	= strrchr($this->GetFilename(), '.');
-		if ($m === false)
-			return null;
-		return substr($m, 1);
-	}
-
-	public function getFilename()
-	{
-		return basename($this->sSrcName);
-	}
-
-	public function isOK()
-	{
-		return $this->iErrorCode == UPLOAD_ERR_OK;
-	}
-
-	public function moveTo($sDestination, $sNewFilename = null)
-	{
-		clearstatcache();
-		Fire(!is_dir($sDestination), 'InvalidArgumentException');
-		Fire(!is_uploaded_file($this->sTmpName), 'UnexpectedValueException');
-
-		if (empty($sNewFilename))
-			$sNewFilename	= $this->getFilename();
-
-		return move_uploaded_file($this->sTmpName, $sDestination . '/' . $sNewFilename);
-	}
-}
+/**
+	A better handling of files uploaded using forms.
+*/
 
 class weeUploadedFiles implements Iterator
 {
-	protected		$bFilterValid;
-	protected		$sFilter;
-	protected		$bChild;
-	protected		$iCurrentChild;
+	/**
+		True if the filter specified by $sFilter is valid, false otherwise.
+		If it is not valid, it will be ignored.
+	*/
+
+	protected $bFilterValid;
+
+	/**
+		Name of the file to filter.
+	*/
+
+	protected $sFilter;
+
+	/**
+		True if we are inside an array of files sharing the same name, false otherwise.
+	*/
+
+	protected $bChild;
+
+	/**
+		Index of the child we are on, if applicable.
+	*/
+
+	protected $iCurrentChild;
+
+	/**
+		Returns a new weeUploadedFile object based on the array given in parameter.
+
+		@param	$a				An array containing the file details.
+		@return	weeUploadedfile	A new file object containing the details given.
+
+		TODO:handle case when $a['name'] is an array
+	*/
 
 	protected function createFile($a)
 	{
-		//TODO:handle case when $a['name'] is an array
 		return new weeUploadedFile($a['name'], $a['tmp_name'], $a['type'], $a['size'], $a['error']);
 	}
+
+	/**
+		Returns a new weeUploadedFile object based on the array given in parameter.
+		This method works on arrays of files of the same name, and returns one of the child.
+		Used when iterating through all the files or an array of files sharing the same name.
+
+		@param	$a				An array containing the file details.
+		@return	weeUploadedfile	A new file object containing the details given.
+	*/
 
 	protected function createFileFromChild($a)
 	{
@@ -128,35 +81,11 @@ class weeUploadedFiles implements Iterator
 			$a['type'][$this->iCurrentChild], $a['size'][$this->iCurrentChild], $a['error'][$this->iCurrentChild]);
 	}
 
-	public function exists($sName)
-	{
-		if (empty($_FILES[$sName]))
-			return false;
+	/**
+		Return the current element.
 
-		if (is_array($_FILES[$sName]['name']))
-			return !empty($_FILES[$sName]['name'][0]);
-		return !empty($_FILES[$sName]['name']);
-	}
-
-	public function filter($sName)
-	{
-		$this->bFilterValid	= array_key_exists($sName, $_FILES);
-		$this->sFilter		= $sName;
-		return $this;
-	}
-
-	public function get($sName)
-	{
-		Fire(empty($_FILES[$sName]) || is_array($_FILES[$sName]['name']), 'InvalidArgumentException');
-		return $this->createFile($_FILES[$sName]);
-	}
-
-	public function isEmpty()
-	{
-		return empty($_FILES);
-	}
-
-	// Iterator
+		@see http://www.php.net/~helly/php/ext/spl/interfaceIterator.html
+	*/
 
 	public function current()
 	{
@@ -172,11 +101,80 @@ class weeUploadedFiles implements Iterator
 		}
 	}
 
+	/**
+		Check if the given file exists.
+
+		@param	$sName	The name of the file.
+		@return	bool	True if the file exists, false otherwise.
+	*/
+
+	public function exists($sName)
+	{
+		if (empty($_FILES[$sName]))
+			return false;
+
+		if (is_array($_FILES[$sName]['name']))
+			return !empty($_FILES[$sName]['name'][0]);
+		return !empty($_FILES[$sName]['name']);
+	}
+
+	/**
+		Filters through the files uploaded.
+		Use filter if you want to iterate only through an array of files sharing the same name.
+
+		@param	$sName	The name of the file(s).
+		@return	$this
+	*/
+
+	public function filter($sName = null)
+	{
+		$this->bFilterValid	= !is_null($sName) && array_key_exists($sName, $_FILES);
+		$this->sFilter		= $sName;
+		return $this;
+	}
+
+	/**
+		Returns the specified file.
+		Throws an exception if the name given points to an array of files.
+
+		@param	$sName	The name of the file.
+		@return	weeUploadedfile	A new file object for the requested file.
+	*/
+
+	public function get($sName)
+	{
+		fire(empty($_FILES[$sName]) || is_array($_FILES[$sName]['name']), 'InvalidArgumentException');
+		return $this->createFile($_FILES[$sName]);
+	}
+
+	/**
+		Checks if there is no uploaded files.
+
+		@return bool True if there is no uploaded files, false otherwise.
+	*/
+
+	public function isEmpty()
+	{
+		return empty($_FILES);
+	}
+
+	/**
+		Return the key of the current element.
+
+		@see http://www.php.net/~helly/php/ext/spl/interfaceIterator.html
+	*/
+
 	public function key()
 	{
 		if (empty($this->sFilter))	return key($_FILES);
 		else						return $this->sFilter;
 	}
+
+	/**
+		Move forward to next element.
+
+		@see http://www.php.net/~helly/php/ext/spl/interfaceIterator.html
+	*/
 
 	public function next()
 	{
@@ -194,9 +192,15 @@ class weeUploadedFiles implements Iterator
 				}
 			}
 			else
-				$this->bFilterValid	= false;
+				$this->bFilterValid = false;
 		}
 	}
+
+	/**
+		Rewind the Iterator to the first element.
+
+		@see http://www.php.net/~helly/php/ext/spl/interfaceIterator.html
+	*/
 
 	public function rewind()
 	{
@@ -213,6 +217,12 @@ class weeUploadedFiles implements Iterator
 			$this->bChild	= is_array($_FILES[$this->sFilter]['name']);
 	}
 
+	/**
+		Check if there is a current element after calls to rewind() or next().
+
+		@see http://www.php.net/~helly/php/ext/spl/interfaceIterator.html
+	*/
+
 	public function valid()
 	{
 		if (empty($this->sFilter))
@@ -224,20 +234,18 @@ class weeUploadedFiles implements Iterator
 					return true;
 				else
 				{
-					$this->bChild	= false;
+					$this->bChild = false;
 					return (next($_FILES) !== false);
 				}
 			}
 
 			return (current($_FILES) !== false);
 		}
-		else
-		{
-			if ($this->bChild)
-				return (array_key_exists($this->iCurrentChild, $_FILES[$this->sFilter]['name']));
-			else
-				return $this->bFilterValid;
-		}
+
+		if ($this->bChild)
+			return (array_key_exists($this->iCurrentChild, $_FILES[$this->sFilter]['name']));
+
+		return $this->bFilterValid;
 	}
 }
 
