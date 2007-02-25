b24c8791c3fe2535d9bbeae013febf789edb6be6
essen <essen@dev-extend.eu>
2007-02-25 17:13:46 +0000

    Untested oracle database driver.

---
+++ wee/db/oracle/weeOracleDatabase.class.php
@@ -0,0 +1,209 @@
+<?php
+
+/*
+	Web:Extend
+	Copyright (c) 2006 Dev:Extend
+
+	This library is free software; you can redistribute it and/or
+	modify it under the terms of the GNU Lesser General Public
+	License as published by the Free Software Foundation; either
+	version 2.1 of the License, or (at your option) any later version.
+
+	This library is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+	Lesser General Public License for more details.
+
+	You should have received a copy of the GNU Lesser General Public
+	License along with this library; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+if (!defined('ALLOW_INCLUSION')) die;
+
+/**
+	Oracle database driver.
+*/
+
+class weeOracleDatabase extends weeDatabase
+{
+	/**
+		Link resource for this database connection.
+	*/
+
+	private $rLink;
+
+	/**
+		Number of affected rows for the previous query.
+		Stocked here to prevent errors if getPKId is called.
+	*/
+
+	private $iNumAffectedRows;
+
+	/**
+		Number of calls to the query method.
+		For informational and debugging purpose only.
+	*/
+
+	private $iNumQueries;
+
+	/**
+		Initialize the driver and connects to the database.
+		The arguments available may change between drivers.
+
+		@param $aParams Arguments for database connection, identification, and class initialization
+	*/
+
+	public function __construct($aParams, $sLogin = null, $sPassword = null)
+	{
+		fire(!function_exists('oci_new_connect'), 'ConfigurationException');
+
+		putenv('NLS_LANG=UTF8');
+		$this->rLink = oci_new_connect(array_value($aParams, 'user'), array_value($aParams, 'password'), array_value($aParams, 'dbname'), 'UTF8');
+		fire($this->rLink === false, 'DatabaseException');
+
+		// Initialize additional database services
+
+		$this->iNumQueries = 0;
+
+		$sPath = dirname(__FILE__);
+		require_once($sPath . '/../weeDatabaseCriteria' . CLASS_EXT);
+		require_once($sPath . '/../weeDatabaseQuery' . CLASS_EXT);
+
+		//TODO:change the criteria class
+		weeDatabaseQuery::$criteriaClass	= 'weeDatabaseCriteria';
+		weeDatabaseQuery::$queryClass		= 'weeDatabaseQuery';
+	}
+
+	/**
+		Close the connection to the database.
+	*/
+
+	public function __destruct()
+	{
+		@oci_close($this->rLink);
+	}
+
+	/**
+		Escape the given value for safe concatenation in an SQL query.
+		You should not build query by concatenation if possible (see query).
+		You should NEVER use sprintf when building queries.
+
+		@param	$mValue	The value to escape
+		@return	string	The escaped value, wrapped around simple quotes
+	*/
+
+	public function escape($mValue)
+	{
+		return "'" . addslashes($mValue) . "'";
+	}
+
+	/**
+		Get the last error the database returned.
+		The drivers usually throw an exception when there's an error,
+		but you can get the error if you catch the exception and then call this method.
+
+		@return string The last error the database encountered
+	*/
+
+	public function getLastError()
+	{
+		$a = oci_error($this->rLink);
+		fire(empty($a), 'IllegalStateException');
+
+		return $a['message'];
+	}
+
+	/**
+		Return the primary key index value.
+		Useful when you need to retrieve the row primary key value you just inserted.
+		This function may work a bit differently in each drivers.
+
+		@param	$sName	The primary key index name, if needed
+		@return	integer	The primary key index value
+	*/
+
+	public function getPKId($sName = null)
+	{
+		fire(empty($sName), 'InvalidParameterException');
+		fire($this->rLink === false, 'IllegalStateException');
+
+		$rStatement = oci_parse($this->rLink, 'SELECT ' . $this->escape($sName) . '.currval FROM DUAL');
+		fire($rStatement === false, 'DatabaseException');
+
+		$b = oci_execute($rStatement, OCI_DEFAULT);
+		fire(!$b, 'DatabaseException');
+
+		$a = oci_fetch_row($rStatement);
+		oci_free_statement($rStatement);
+
+		return $a[0];
+	}
+
+	/**
+		Return the number of affected rows in the last INSERT, UPDATE or DELETE query.
+		You can't use this method safely to check if your UPDATE executed successfully,
+		since the UPDATE statement does not always update rows that are already up-to-date.
+
+		@return integer The number of affected rows in the last query
+	*/
+
+	public function numAffectedRows()
+	{
+		fire($this->rLink === false, 'IllegalStateException');
+		return $this->iNumAffectedRows;
+	}
+
+	/**
+		Return the number of successfull queries.
+		Only the queries executed using the query method are recorded.
+		For informational and debugging purpose only.
+
+		@return integer The number of queries since the creation of the class
+	*/
+
+	public function numQueries()
+	{
+		return $this->iNumQueries;
+	}
+
+	/**
+		Execute an SQL query.
+
+		If you pass other arguments to it, the arguments will be escaped and inserted into the query,
+		using the buildSafeQuery method.
+
+		For example if you have:
+			$Db->query('SELECT ? FROM example_table WHERE example_id=? LIMIT 1', $sField, $iId);
+		It will select the $sField field from the row with the $iId example_id.
+
+		@overload query($mQueryString, $mArg1, $mArg2, ...) Example of query call with multiple arguments
+		@param	$mQueryString		The query string
+		@param	...					The additional arguments that will be inserted into the query
+		@return	weeDatabaseResult	Only with SELECT queries: an object for results handling
+	*/
+
+	public function query($mQueryString)
+	{
+		fire($this->rLink === false, 'IllegalStateException');
+
+		$this->iNumQueries++;
+
+		if (func_num_args() > 1)
+			$mQueryString = $this->buildSafeQuery(func_get_args());
+		elseif (is_object($mQueryString))
+			$mQueryString = $mQueryString->build($this);
+
+		$rStatement = oci_parse($this->rLink, $mQueryString);
+		fire($rStatement === false, 'DatabaseException');
+
+		$b = oci_execute($rStatement, OCI_DEFAULT);
+		fire(!$b, 'DatabaseException');
+
+		//TODO:probably don't work like this
+		if (oci_num_rows($rStatement) > 0)
+			return new weeOracleResult($rStatement);
+	}
+}
+
+?>
---
+++ wee/db/oracle/weeOracleResult.class.php
@@ -0,0 +1,190 @@
+<?php
+
+/*
+	Web:Extend
+	Copyright (c) 2006 Dev:Extend
+
+	This library is free software; you can redistribute it and/or
+	modify it under the terms of the GNU Lesser General Public
+	License as published by the Free Software Foundation; either
+	version 2.1 of the License, or (at your option) any later version.
+
+	This library is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+	Lesser General Public License for more details.
+
+	You should have received a copy of the GNU Lesser General Public
+	License along with this library; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+if (!defined('ALLOW_INCLUSION')) die;
+
+/**
+	Class for Oracle query results handling.
+	An object of this class is created by the weeOracleDatabase's query method for SELECT statements.
+*/
+
+class weeOracleResult extends weeDatabaseResult
+{
+	/**
+		Resource for this query result.
+	*/
+
+	private $rResult;
+
+	/**
+		Data from the current row.
+	*/
+
+	private $aCurrentFetch;
+
+	/**
+		Index number of the row to fetch.
+		Second parameter of pg_fetch_assoc.
+	*/
+
+	private $iCurrentIndex;
+
+	/**
+		Initialize the class with the result of the query.
+
+		@param $rResult The resource for the query result returned by weeDatabase's query method.
+	*/
+
+	public function __construct($rResult)
+	{
+		fire(!is_resource($rResult), 'InvalidArgumentException');
+		$this->rResult = $rResult;
+	}
+
+	/**
+		Return the current row.
+
+		@see http://www.php.net/~helly/php/ext/spl/interfaceIterator.html
+	*/
+
+	public function current()
+	{
+		return $this->processRow($this->aCurrentFetch);
+	}
+
+	/**
+		Delete the resource and clean up space and memory.
+	*/
+
+	public function __destruct()
+	{
+		oci_free_statement($this->rResult);
+	}
+
+	/**
+		Fetch the next row.
+
+		Usually used to fetch the result of a query with only one result returned,
+		because if there's no result it throws an exception.
+
+		The return value type can differ depending on the row class.
+		The row class can be changed using the rowClass method.
+
+		@return array Usually an array, or a child of weeDatabaseRow.
+	*/
+
+	public function fetch()
+	{
+		$a = oci_fetch_assoc($this->rResult);
+		fire($a === false, 'DatabaseException');
+
+		if (!empty($this->sRowClass))
+			$a = new $this->sRowClass($a);
+
+		return $this->processRow($a);
+	}
+
+	/**
+		Fetch all the rows returned by the query.
+
+		The return value type can differ depending on the row class.
+		The row class can be changed using the rowClass method.
+
+		@return array Usually an array, or a child of weeDatabaseRow.
+	*/
+
+	public function fetchAll()
+	{
+		//TODO:handle the row class here too, and don't fire
+		fire(!empty($this->sRowClass), 'IllegalStateException');
+
+		oci_fetch_all($this->rResult, $aAll, 0, -1, OCI_ASSOC);
+		return $aAll;
+	}
+
+	/**
+		Return the key of the current row.
+
+		@see http://www.php.net/~helly/php/ext/spl/interfaceIterator.html
+	*/
+
+	public function key()
+	{
+		return $this->iCurrentIndex;
+	}
+
+	/**
+		Move forward to next row.
+
+		@see http://www.php.net/~helly/php/ext/spl/interfaceIterator.html
+	*/
+
+	public function next()
+	{
+		$this->iCurrentIndex++;
+	}
+
+	/**
+		Return the number of results returned by the query.
+
+		@return int The number of results.
+	*/
+
+	public function numResults()
+	{
+		$i = oci_num_rows($this->rResult);
+		fire($i === false, 'DatabaseException');
+
+		return $i;
+	}
+
+	/**
+		Rewind the Iterator to the first row.
+
+		@see http://www.php.net/~helly/php/ext/spl/interfaceIterator.html
+	*/
+
+	public function rewind()
+	{
+		// Oracle can't rewind results. We need to find another way to do that
+		fire(!empty($this->iCurrentIndex), 'DatabaseException');
+
+		$this->iCurrentIndex = 0;
+	}
+
+	/**
+		Check if there is a current row after calls to rewind() or next().
+
+		@see http://www.php.net/~helly/php/ext/spl/interfaceIterator.html
+	*/
+
+	public function valid()
+	{
+		$this->aCurrentFetch = oci_fetch_assoc($this->rResult, $this->iCurrentIndex);
+
+		if (!empty($this->sRowClass) && $this->aCurrentFetch !== false)
+			$this->aCurrentFetch = new $this->sRowClass($this->aCurrentFetch);
+
+		return ($this->aCurrentFetch !== false);
+	}
+}
+
+?>
