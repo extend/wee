4ec0e08f107ea821181101378d756e46f71f389b
essen <essen@dev-extend.eu>
2007-12-10 19:00:28 +0000

    Updated the path to the default configuration file in the comment, which was wrong since it was recently changed.

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 
 // You can define a different WEE_CONF_FILE here
-// define('WEE_CONF_FILE', 'conf/wee.cnf');
-// Defaults to conf/wee.cnf
+// define('WEE_CONF_FILE', 'app/conf/wee.cnf');
+// Defaults to app/conf/wee.cnf
 
 define('ALLOW_INCLUSION', 1);
 require('wee/wee.php');
