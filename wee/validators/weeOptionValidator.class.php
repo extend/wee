16c4ad156510f111d9f953a03064f2943fd6282c
essen <essen@dev-extend.eu>
2007-12-26 20:13:19 +0000

    Added a missing error message for an exception.

--- wee/validators/weeOptionValidator.class.php
+++ wee/validators/weeOptionValidator.class.php
@@ -99,7 +99,8 @@ class weeOptionValidator implements weeFormValidator
 	{
 		fire(empty($this->oWidget), 'InvalidStateException',
 			'You must set the widget using weeOptionValidator::setWidget before calling this method.');
-		fire($this->oWidget instanceof weeFormMultipleSelectable, 'InvalidArgumentException'); //TODO:error message?
+		fire($this->oWidget instanceof weeFormMultipleSelectable, 'InvalidArgumentException',
+			'weeOptionValidator must be used only on weeFormMultipleSelectable widgets.');
 
 		if (!$this->oWidget->isInOptions($this->mValue))
 			$this->setError('invalid');
