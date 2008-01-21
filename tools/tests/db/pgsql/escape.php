b1a5efa0eb7ad73616b04d8ca84c95b6dc2dc8ac
essen <essen@dev-extend.eu>
2008-01-21 11:07:49 +0000

    Added unit tests for the weePgSQLDatabase::escape method.

---
+++ tools/tests/db/pgsql/escape.php
@@ -0,0 +1,28 @@
+<?php
+
+// Connect
+
+$oDb = new weePgSQLDatabase(array(
+	'host'		=> 'localhost',
+	'user'		=> 'wee_tests',
+	'password'	=> 'wee_tests',
+	'dbname'	=> 'wee_tests',
+));
+
+// Test the method weePgSQLDatabase::escape
+
+$this->isEqual("'egg'", $oDb->escape('egg'),
+	"Escaping of the string 'egg' is wrong.");
+$this->isEqual("'123'", $oDb->escape(123),
+	'Escaping of the integer 123 is wrong.');
+$this->isEqual("'7.5'", $oDb->escape(7.5),
+	'Escaping of the float 7.5 is wrong.');
+$this->isEqual("'7.5'", $oDb->escape(7.50),
+	'Escaping of the float 7.50 is wrong.');
+
+$this->isEqual("'that''s all folks!'", $oDb->escape("that's all folks!"),
+	'Escaping of the string "that\'s all folks" is wrong.');
+
+// Clean up
+
+unset($oDb);
