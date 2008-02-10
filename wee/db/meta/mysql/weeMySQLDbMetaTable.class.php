5ac49e9c9f628db1fe57ae28fddcadd734013c26
nox <nox@dev-extend.eu>
2008-02-10 08:55:58 +0000

    weeMySQLDbMetaTable improvements:  * Use table_rows to count the table when the engine is not InnoDB.  * comment() and charset().  * Integer fields correctly cast to int.

--- wee/db/meta/mysql/weeMySQLDbMetaTable.class.php
+++ wee/db/meta/mysql/weeMySQLDbMetaTable.class.php
@@ -42,6 +42,87 @@ class weeMySQLDbMetaTable extends weeDbMetaTable
 	public function __construct(weeMySQLDbMeta $oMeta, array $aInfos)
 	{
 		parent::__construct($oMeta, $aInfos);
+
+		// These are integers...
+		$this->aInfos['version']		= (int) $this->aInfos['version'];
+		$this->aInfos['table_rows']		= (int) $this->aInfos['table_rows'];
+
+		// ...but auto_increment can be null.
+		if ($this->aInfos['auto_increment'] !== null)
+			$this->aInfos['auto_increment']	= (int) $this->aInfos['auto_increment'];
+	}
+
+	/**
+		Returns the character set of the table.
+
+		@return	string	The character set.
+	*/
+
+	public function charset()
+	{
+		return substr($this->aInfos['table_collation'], 0,
+			strpos($this->aInfos['table_collation'], '_'));
+	}
+
+	/**
+		Returns the comment of the table.
+
+		@return	string	The comment of the table.
+	*/
+
+	public function comment()
+	{
+		return $this->aInfos['table_comment'];
+	}
+
+	/**
+		Returns the number of rows in the table.
+
+		@see	http://www.php.net/~helly/php/ext/spl/interfaceCountable.html
+	*/
+
+	public function count()
+	{
+		// table_rows is the number of rows in the table, but it is only a rough
+		// approximation if the storage engine is InnoDB.
+
+		if ($this->aInfos['engine'] == 'InnoDB')
+			return parent::count();
+
+		return $aCount['table_rows'];
+	}
+
+	/**
+		Returns the array of custom offsets reachable through ArrayAccess interface.
+		This class defines two new offsets:
+			- charset:	The character set of the table.
+			- comment:	The comment of the table.
+
+		@return	array	The array of custom offsets.
+	*/
+
+	protected static function getCustomOffsets()
+	{
+		return array_merge(parent::getCustomOffsets(),
+			array('charset', 'comment'));
+	}
+
+	/**
+		Returns the value of a custom offset.
+
+		@param	$sOffset	The custom offset.
+		@return mixed		The value associated with the custom offset.
+	*/
+
+	protected function getCustomOffset($sOffset)
+	{
+		switch ($sOffset)
+		{
+			case 'charset': return $this->charset();
+			case 'comment': return $this->comment();
+		}
+
+		return parent::getCustomOffset($sOffset);
 	}
 
 	/**
@@ -53,7 +134,12 @@ class weeMySQLDbMetaTable extends weeDbMetaTable
 	public static function getFields()
 	{
 		return array_merge(parent::getFields(), array(
-			));
+			'engine',
+			'version',
+			'table_rows',
+			'auto_increment',
+			'table_collation',
+			'table_comment'));
 	}
 
 	/**
