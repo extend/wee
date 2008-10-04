8f63e853d9df9a70a8212b6a156993fc4e7ad1d8
yl <yl@dev-extend.eu>
2008-10-04 17:04:45 +0000

    Added tests for weeGetTextDictionary and weeGetTextReader.

---
+++ tools/tests/l10n/gettextdictionary.php
@@ -0,0 +1,105 @@
+<?
+
+$sPoFilename	= ROOT_PATH . 'tools/tests/l10n/messages.po';
+$sMoFilename	= ROOT_PATH . 'tools/tests/l10n/messages.mo';
+$aHeaders 		= array('Content-Type' => 'text/plain; charset=UTF-8', 'Plural-Forms' => 'nplurals=2; plural=(n>1);');
+$aTranslation	= array('Wrong password' => 'Mot de passe incorrect');
+
+$sPoContents	= '#default domain "messages.mo"
+msgid ""
+msgstr ""
+"Content-Type: text/plain; charset=UTF-8\n"
+"Plural-Forms: nplurals=2; plural=(n>1);\n"
+
+msgid "Wrong password"
+msgstr "Mot de passe incorrect"
+
+msgid "Wrong number"
+msgid_plural "Wrongs numbers"
+msgstr[0] "Mauvais chiffre"
+msgstr[1] "Mauvais chiffres"';
+
+$iWrote = file_put_contents($sPoFilename, $sPoContents);
+$iWrote === false and burn('UnexpectedValueException', sprintf(_('Cannot write the file %s.'), $sPoFilename));
+
+exec(sprintf('msgfmt -o %s %s', $sMoFilename, $sPoFilename));
+
+try {
+	$o = new weeGetTextDictionary($sMoFilename);
+
+	$aMoHeaders = $o->getHeaders();
+
+	$this->isEqual($aMoHeaders, $aHeaders, _('weeGetTextDictionary::getHeaders should return 
+		[Content-Type] => text/plain; charset=UTF-8 
+		[Plural-Forms] => nplurals=2; plural=(n>1);'));
+
+	$sCharset = $o->getCharset();
+	$this->isEqual($sCharset, 'UTF-8', sprintf(_('weeGetTextDictionary::getCharset should return UTF-8 got "%s" instead'), $sCharset));
+
+	$sString = $o->getTranslation('Wrong password');
+	$this->isEqual($sString, 'Mot de passe incorrect',
+		sprintf(_('weeGetTextDictionary::getCharset should return "Mot de passe incorrect" got "%s" instead'), $sString));
+
+} catch (UnexpectedValueException $e) {
+	$this->fail(sprintf(_('weeGetTextDictionary should not throw an UnexpectedValueException when trying to read the file %s.'), $sMoFilename));
+}
+
+try {
+	$o = new weeGetTextDictionary($sMoFilename);
+
+	$sTranslation = $o->getPluralTranslation('Wrong number', 'Wrongs numbers', 0);
+	$this->isEqual($sTranslation, 'Mauvais chiffres',
+		sprintf(_('weeGetTextDictionary::getPluralTranslation should return "Mauvais chiffres" got "%s" instead'), $sTranslation));	
+	$sTranslation = $o->getPluralTranslation('Wrong number', 'Wrongs numbers', 1);
+	$this->isEqual($sTranslation, 'Mauvais chiffre',
+		sprintf(_('weeGetTextDictionary::getPluralTranslation should return "Mauvais chiffre" got "%s" instead'), $sTranslation));
+	$sTranslation = $o->getPluralTranslation('Wrong number', 'Wrongs numbers', 2);
+	$this->isEqual($sTranslation, 'Mauvais chiffres',
+		sprintf(_('weeGetTextDictionary::getPluralTranslation should return "Mauvais chiffres" got "%s" instead'), $sTranslation));
+	$sTranslation = $o->getPluralTranslation('Wrong number', 'Wrongs numbers', 10);
+	$this->isEqual($sTranslation, 'Mauvais chiffres',
+		sprintf(_('weeGetTextDictionary::getPluralTranslation should return "Mauvais chiffres" got "%s" instead'), $sTranslation));
+
+	$sTranslation = $o->getPluralTranslation('pouet', 'Wrongs numbers', 0);
+	$this->isEqual($sTranslation, 'Wrongs numbers',
+		sprintf(_('weeGetTextDictionary::getPluralTranslation should return "Wrongs numbers" got "%s" instead'), $sTranslation));
+	$sTranslation = $o->getPluralTranslation('pouet', 'Wrongs numbers', 1);
+	$this->isEqual($sTranslation, 'Wrongs numbers',
+		sprintf(_('weeGetTextDictionary::getPluralTranslation should return "Wrongs numbers" got "%s" instead'), $sTranslation));
+	$sTranslation = $o->getPluralTranslation('pouet', 'Wrongs numbers', 2);
+	$this->isEqual($sTranslation, 'pouet',
+		sprintf(_('weeGetTextDictionary::getPluralTranslation should return "pouet" got "%s" instead'), $sTranslation));
+	$sTranslation = $o->getPluralTranslation('pouet', 'Wrongs numbers', 10);
+	$this->isEqual($sTranslation, 'pouet',
+		sprintf(_('weeGetTextDictionary::getPluralTranslation should return "pouet" got "%s" instead'), $sTranslation));
+
+	$sTranslation = $o->getPluralTranslation('Wrongs numbers', 'pouet', 0);
+	$this->isEqual($sTranslation, 'pouet',
+		sprintf(_('weeGetTextDictionary::getPluralTranslation should return "pouet" got "%s" instead'), $sTranslation));
+	$sTranslation = $o->getPluralTranslation('Wrong number', 'pouet', 1);
+	$this->isEqual($sTranslation, 'pouet',
+		sprintf(_('weeGetTextDictionary::getPluralTranslation should return "pouet" got "%s" instead'), $sTranslation));
+	$sTranslation = $o->getPluralTranslation('Wrong number', 'pouet', 2);
+	$this->isEqual($sTranslation, 'Wrong number',
+		sprintf(_('weeGetTextDictionary::getPluralTranslation should return "Wrong number" got "%s" instead'), $sTranslation));
+	$sTranslation = $o->getPluralTranslation('Wrong number', 'pouet', 10);
+	$this->isEqual($sTranslation, 'Wrong number',
+		sprintf(_('weeGetTextDictionary::getPluralTranslation should return "Wrong number" got "%s" instead'), $sTranslation));
+
+	$sTranslation = $o->getPluralTranslation('pouet', 'pouet', 0);
+	$this->isEqual($sTranslation, 'pouet',
+		sprintf(_('weeGetTextDictionary::getPluralTranslation should return "pouet" got "%s" instead'), $sTranslation));
+	$sTranslation = $o->getPluralTranslation('pouet', 'pouet', 1);
+	$this->isEqual($sTranslation, 'pouet',
+		sprintf(_('weeGetTextDictionary::getPluralTranslation should return "pouet" got "%s" instead'), $sTranslation));
+	$sTranslation = $o->getPluralTranslation('pouet', 'pouet', 2);
+	$this->isEqual($sTranslation, 'pouet',
+		sprintf(_('weeGetTextDictionary::getPluralTranslation should return "pouet" got "%s" instead'), $sTranslation));
+	$sTranslation = $o->getPluralTranslation('pouet', 'pouet', 10);
+	$this->isEqual($sTranslation, 'pouet',
+		sprintf(_('weeGetTextDictionary::getPluralTranslation should return "pouet" got "%s" instead'), $sTranslation));
+
+} catch (UnexceptedValueException $e) {}
+
+unlink($sPoFilename);
+unlink($sMoFilename);
---
+++ tools/tests/l10n/gettextreader.php
@@ -0,0 +1,50 @@
+<?
+
+$sFilename		= ROOT_PATH . 'tools/tests/l10n/file.txt';
+$sPoFilename	= ROOT_PATH . 'tools/tests/l10n/messages.po';
+$sMoFilename	= ROOT_PATH . 'tools/tests/l10n/messages.mo';
+$sPoContents	= '# default domain "messages.mo"
+msgid  "message : ad vitam aeternam"
+msgstr "translation : forever"';
+
+touch($sFilename);
+chmod($sFilename, 0644);
+
+$iWrote = file_put_contents($sFilename, 'word');
+$iWrote === false and burn('UnexpectedValueException', sprintf(_('Cannot write the file %s.'), $sFilename));
+
+try {
+	$o = new weeGetTextReader($sFilename);
+	$this->fail(sprintf(_('weeGetTextReader should throw an UnexpectedValueException when reading %s.'), $sFilename));
+} catch (UnexpectedValueException $e) {}
+
+touch($sPoFilename);
+chmod($sPoFilename, 0655);
+
+$iWrote = file_put_contents($sPoFilename, $sPoContents);
+$iWrote === false and burn('UnexpectedValueException', sprintf(_('Cannot write the file %s.'), $sPoFilename));
+
+exec(sprintf('msgfmt -o %s %s', $sMoFilename, $sPoFilename));
+
+try {
+	$o = new weeGetTextReader($sMoFilename);
+} catch (UnexpectedValueException $e) {
+	$this->fail(sprintf(_('weeGetTextReader should not throw an UnexpectedValueException when trying to read the file %s.'), $sMoFilename));
+}
+
+$aExpectedResult = array ('message : ad vitam aeternam' => 'translation : forever');
+$aResult = array();
+
+try {
+	$o = new weeGetTextReader($sMoFilename);
+	$aResult = $o->getStrings();
+	$this->isEqual($aExpectedResult, $aResult,
+		sprintf(_('weeGetTextReader::getStrings failed, expected result : "%s=>%s", got "%s=>%s" instead.'),
+		key($aExpectedResult), $aExpectedResult[key($aExpectedResult)], key($aResult), $aResult[key($aResult)]));
+} catch (UnexpectedValueException $e) {
+	$this->fail(sprintf(_('weeGetTextReader should not throw an UnexpectedValueException when trying to read the file %s.'), $sMoFilename));
+}
+
+unlink($sFilename);
+unlink($sMoFilename);
+unlink($sPoFilename);
