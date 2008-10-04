3f5157cf5d23c06e116ed8909ef3c7a703bdd7b7
yl <yl@dev-extend.eu>
2008-10-04 17:06:20 +0000

    Replace an fopen/fwrite/fclose by a fileputcontents.

--- tools/tests/stream/filestream.php
+++ tools/tests/stream/filestream.php
@@ -1,53 +1,51 @@
 <?
 
-$sFileNameNotExist	= 'file_which_does_not_exist.txt';
-$sFileNameExist		= ROOT_PATH . 'tools/tests/stream/file.txt';
+$sFilenameNotExist	= 'file_which_does_not_exist.txt';
+$sFilenameExist		= ROOT_PATH . 'tools/tests/stream/file.txt';
 
-touch($sFileNameExist);
-chmod($sFileNameExist, 0644);
+touch($sFilenameExist);
+chmod($sFilenameExist, 0644);
 
-$rHandle = fopen($sFileNameExist, 'w');
-$rHandle === false and burn('UnexpectedValueException', sprintf(_('Cannot open file %s.'), $sFileNameExist));
-
-$iWrote = fwrite($rHandle, 'some words');
-$iWrote === false and burn('UnexpectedValueException', sprintf(_('Cannot write the file %s.'), $sFileNameExist));
-
-fclose($rHandle);
+$iWrote = file_put_contents($sFilenameExist, 'some words');
+$iWrote === false and burn('UnexpectedValueException', sprintf(_('Cannot write the file %s.'), $sFilenameExist));
 
 try {
-	$oFileStream = new weeFileStream($sFileNameNotExist);
-	$this->fail(sprintf(_('weeFileStream should throw a FileNotFoundException when trying to access %s.'), $sFileNameNotExist));
+	$oFileStream = new weeFileStream($sFilenameNotExist);
+	$this->fail(sprintf(_('weeFileStream should throw a FileNotFoundException when trying to access %s.'), $sFilenameNotExist));
 } catch (FileNotFoundException $e) {}
 
 // The file exists but has not reading permission
-chmod($sFileNameExist, 0000);
+chmod($sFilenameExist, 0000);
 try {
-	$oFileStream = new weeFileStream($sFileNameExist);
-	$this->fail(sprintf(_('weeFileStream should throw a NotPermittedException when trying to access %s'), $sFileNameExist));
+	$oFileStream = new weeFileStream($sFilenameExist);
+	$this->fail(sprintf(_('weeFileStream should throw a NotPermittedException when trying to access %s'), $sFilenameExist));
 } catch (NotPermittedException $e) {}
-chmod($sFileNameExist, 0644);
 
+chmod($sFilenameExist, 0644);
 try {
-	$oFileStream = new weeFileStream($sFileNameExist);
-	$oFileStream->read(filesize($sFileNameExist) + 32);
-	$this->fail(sprintf(_('read should throw an EndOfFileException when trying to read more than the size of %s.'), $sFileNameExist));
+	$oFileStream = new weeFileStream($sFilenameExist);
+
+	$oFileStream->read(filesize($sFilenameExist) + 32);
+	$this->fail(sprintf(_('read should throw an EndOfFileException when trying to read more than the size of %s.'), $sFilenameExist));
 } catch (EndOfFileException $e) {}
 
 // Seeking past EOF is not considered an error.
 try {
-	$oFileStream = new weeFileStream($sFileNameExist);
+	$oFileStream = new weeFileStream($sFilenameExist);
+
 	$oFileStream->seek(-1);
-	$this->fail(sprintf(_('seek should throw an EndOfFileException when trying to seek before the beginning of the file %s.'), $sFileNameExist));
+	$this->fail(sprintf(_('seek should throw an EndOfFileException when trying to seek before the beginning of the file %s.'), $sFilenameExist));
 } catch (EndOfFileException $e) {}
 
 try {
-	$oFileStream = new weeFileStream($sFileNameExist);
+	$oFileStream = new weeFileStream($sFilenameExist);
+
 	$oFileStream->seek(5);
 	$s = $oFileStream->read(5);
 	$this->isEqual($s, 'words',
 		sprintf(_("read should return the word 'words', got '%s' instead."), $s));
 } catch (EndOfFileException $e) {
-	$this->fail(sprintf(_('read should not throw an EndOfFileException when trying to seek/read the file %s.'), $sFileNameExist));
+	$this->fail(sprintf(_('read should not throw an EndOfFileException when trying to seek/read the file %s.'), $sFilenameExist));
 }
 
-unlink($sFileNameExist);
+unlink($sFilenameExist);
