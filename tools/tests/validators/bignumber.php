df5a58bebc8d44afb185563cce7fe438100ec198
nox <nox@dev-extend.eu>
2008-10-25 13:56:31 +0000

    Added weeBigNumberValidator for... you know... BIG numbers.

---
+++ tools/tests/validators/bignumber.php
@@ -0,0 +1,215 @@
+<?php
+
+class PrintableInput_testBigNumberValidator implements Printable {
+	public function toString() {
+		return '42';
+	}
+}
+
+class CastableInput_testBigNumberValidator {
+	public function __toString() {
+		return '42';
+	}
+}
+
+// weeBigNumberValidator should throw a DomainException when the value to validate is neither an integer,
+// a float or a string, an instance of Printable or an object castable to string.
+
+try {
+	new weeBigNumberValidator(new stdClass);
+	$this->fail(_('weeBigNumberValidator should throw a DomainException when the value is an object which is neither an instance of Printable nor an object castable to string.'));
+} catch (DomainException $e) {}
+
+try {
+	new weeBigNumberValidator(true);
+	$this->fail(_('weeBigNumberValidator should throw a DomainException when the value is a boolean.'));
+} catch (DomainException $e) {}
+
+try {
+	new weeBigNumberValidator(null);
+	$this->fail(_('weeBigNumberValidator should throw a DomainException when the value is null.'));
+} catch (DomainException $e) {}
+
+try {
+	new weeBigNumberValidator(array());
+	$this->fail(_('weeBigNumberValidator should throw a DomainException when the value is an array.'));
+} catch (DomainException $e) {}
+
+try {
+	new weeBigNumberValidator(42);
+	$this->fail(_('weeBigNumberValidator should throw a DomainException when the value is an integer.'));
+} catch (DomainException $e) {
+}
+
+try {
+	new weeBigNumberValidator(42.42);
+	$this->fail(_('weeBigNumberValidator should throw a DomainException when the value is a float.'));
+} catch (DomainException $e) {
+}
+
+try {
+	new weeBigNumberValidator('42');
+} catch (DomainException $e) {
+	$this->fail(_('weeBigNumberValidator should not throw a DomainException when the value is a string.'));
+}
+
+try {
+	new weeBigNumberValidator(new PrintableInput_testBigNumberValidator);
+} catch (DomainException $e) {
+	$this->fail(_('weeBigNumberValidator should not throw a DomainException when the value is an instance of Printable.'));
+}
+
+try {
+	new weeBigNumberValidator(new CastableInput_testBigNumberValidator);
+} catch (DomainException $e) {
+	$this->fail(_('weeBigNumberValidator should not throw a DomainException when the value is an object castable to string.'));
+}
+
+// weeBigNumberValidator should throw a DomainException when the `min` argument is not a valid number.
+
+try {
+	new weeBigNumberValidator('42', array('min' => 'not_a_number'));
+	$this->fail(_('weeBigNumberValidator should throw a DomainException when the `min` argument is not a valid number.'));
+} catch (DomainException $e) {}
+
+try {
+	new weeBigNumberValidator('42', array('min' => 42));
+	$this->fail(_('weeBigNumberValidator should throw a DomainException when the `min` argument is not given as a string.'));
+} catch (DomainException $e) {}
+
+try {
+	new weeBigNumberValidator('42', array('min' => '42'));
+} catch (DomainException $e) {
+	$this->fail(_('weeBigNumberValidator should not throw a DomainException when the `min` argument is a valid number.'));
+}
+
+// weeBigNumberValidator should throw a DomainException when the `max` argument is not a valid number.
+
+try {
+	new weeBigNumberValidator('42', array('max' => 'not_a_number'));
+	$this->fail(_('weeBigNumberValidator should throw a DomainException when the `max` argument is not a number.'));
+} catch (DomainException $e) {}
+
+try {
+	new weeBigNumberValidator('42', array('max' => 42));
+	$this->fail(_('weeBigNumberValidator should throw a DomainException when the `max` argument is not given as a string.'));
+} catch (DomainException $e) {}
+
+try {
+	new weeBigNumberValidator('42', array('max' => '42'));
+} catch (DomainException $e) {
+	$this->fail(_('weeBigNumberValidator should not throw a DomainException when the `max` argument is a valid number.'));
+}
+
+// weeBigNumberValidator should throw an InvalidArgumentException when the `min` and `max` arguments do not form a valid number range.
+
+try {
+	new weeBigNumberValidator('42', array('min' => '43', 'max' => '41'));
+	$this->fail(_('weeBigNumberValidator should throw an InvalidArgumentException when the `min` argument is greater than the `max` one.'));
+} catch (InvalidArgumentException $e) {}
+
+try {
+	new weeBigNumberValidator('42', array('min' => '42', 'max' => '42'));
+	$this->fail(_('weeBigNumberValidator should throw an InvalidArgumentException when the `min` and `max` arguments are equal.'));
+} catch (InvalidArgumentException $e) {}
+
+try {
+	new weeBigNumberValidator('42', array('min' => '41', 'max' => '43'));
+} catch (InvalidArgumentException $e) {
+	$this->fail(_('weeBigNumberValidator should not throw an InvalidArgumentException when the `min` argument is smaller than the `max` one.'));
+}
+
+// Integer (string)
+
+$this->isTrue(weeBigNumberValidator::test('656'),
+	'weeBigNumberValidator fails to validate the string "656".');
+$this->isFalse(weeBigNumberValidator::test('1.0'),
+	'weeBigNumberValidator returns true for the string "1.0" when it should validate integers only.');
+$this->isFalse(weeBigNumberValidator::test('1.1'),
+	'weeBigNumberValidator returns true for the string "1.1" when it should validate integers only.');
+
+// Float (string)
+
+$this->isTrue(weeBigNumberValidator::test('0', array('format' => 'float')),
+	'weeBigNumberValidator fails to validate the string "0" when it should validate both integer and float.');
+$this->isTrue(weeBigNumberValidator::test('0.0', array('format' => 'float')),
+	'weeBigNumberValidator fails to validate the string "0.0" when it should validate both integer and float.');
+$this->isTrue(weeBigNumberValidator::test('1', array('format' => 'float')),
+	'weeBigNumberValidator fails to validate the string "1" when it should validate both integer and float.');
+$this->isTrue(weeBigNumberValidator::test('1.1', array('format' => 'float')),
+	'weeBigNumberValidator fails to validate the string "1.1" when it should validate both integer and float.');
+$this->isFalse(weeBigNumberValidator::test('1.1.1', array('format' => 'float')),
+	'weeBigNumberValidator returns true for the string "1.1.1".');
+
+// Bad values
+
+$this->isFalse(weeBigNumberValidator::test(''),
+	'weeBigNumberValidator returns true for the empty string.');
+$this->isFalse(weeBigNumberValidator::test('32f'),
+	'weeBigNumberValidator returns true for the string "32f".');
+$this->isFalse(weeBigNumberValidator::test('xxx'),
+	'weeBigNumberValidator returns true for the string "xxx".');
+
+// Integer min/max
+
+$this->isTrue(weeBigNumberValidator::test('0', array('min' => '-10')),
+	'weeBigNumberValidator fails to validate 0 >= -10.');
+$this->isFalse(weeBigNumberValidator::test('0', array('min' => '10')),
+	'weeBigNumberValidator returns true for 0 >= 10.');
+$this->isTrue(weeBigNumberValidator::test('0', array('max' => '10')),
+	'weeBigNumberValidator fails to validate 0 <= 10.');
+$this->isFalse(weeBigNumberValidator::test('0', array('max' => '-10')),
+	'weeBigNumberValidator returns true for 0 <= -10.');
+
+// Float min/max
+
+$this->isTrue(weeBigNumberValidator::test('1.1', array('format' => 'float', 'min' => '0')),
+	'weeBigNumberValidator fails to validate 1.1 >= 0.');
+$this->isFalse(weeBigNumberValidator::test('1.1', array('format' => 'float', 'min' => '2')),
+	'weeBigNumberValidator returns true for 1.1 >= 2.');
+$this->isFalse(weeBigNumberValidator::test('1.1', array('format' => 'float', 'max' => '0')),
+	'weeBigNumberValidator returns true for 1.1 <= 0.');
+$this->isTrue(weeBigNumberValidator::test('1.1', array('format' => 'float', 'max' => '2')),
+	'weeBigNumberValidator fails to validate 1.1 <= 2.');
+$this->isTrue(weeBigNumberValidator::test('1.1', array('format' => 'float', 'min' => '1.0')),
+	'weeBigNumberValidator fails to validate 1.1 >= 1.0.');
+$this->isFalse(weeBigNumberValidator::test('1.1', array('format' => 'float', 'min' => '1.2')),
+	'weeBigNumberValidator returns true for 1.1 >= 1.2.');
+$this->isFalse(weeBigNumberValidator::test('1.1', array('format' => 'float', 'max' => '1.0')),
+	'weeBigNumberValidator returns true for 1.1 <= 1.0.');
+$this->isTrue(weeBigNumberValidator::test('1.1', array('format' => 'float', 'max' => '1.2')),
+	'weeBigNumberValidator fails to validate 1.1 <= 1.2.');
+
+// Big Numbers
+
+$this->isTrue(weeBigNumberValidator::test('12345678901234567890'),
+	_('weeBigNumberValidator::test should return true for a valid big number'));
+$this->isTrue(weeBigNumberValidator::test('-12345678901234567890'),
+	_('weeBigNumberValidator::test should return true for a valid negative big number'));
+$this->isTrue(weeBigNumberValidator::test('12345678901234567890.000042', array('format' => 'float')),
+	_('weeBigNumberValidator::test should return true for a valid float big number'));
+$this->isTrue(weeBigNumberValidator::test('12345678901234567890.000042', array('format' => 'float', 'min' => '12345678901234567890')),
+	_('weeBigNumberValidator::test should return true for a valid float big number greater than the `min` argument.'));
+$this->isFalse(weeBigNumberValidator::test('12345678901234567890.000042', array('format' => 'float', 'max' => '12345678901234567890')),
+	_('weeBigNumberValidator::test should return false for a valid float big number less than the `max` argument.'));
+$this->isTrue(weeBigNumberValidator::test('-0.000000000000000000001', array('format' => 'float', 'min' => '-0.000000000000000000002')),
+	_('weeBigNumberValidator::test should return true for a valid negative float big number greater than the `min` argument.'));
+
+// The value is outside the range.
+
+$this->isFalse(weeBigNumberValidator::test('42', array('min' => '43', 'max' => '44')),
+	_('weeBigNumberValidator should return false if the value is under the range of the `min` and `max` arguments.'));
+
+$this->isFalse(weeBigNumberValidator::test('42', array('min' => '40', 'max' => '41')),
+	_('weeBigNumberValidator should return false if the value is over the range of the `min` and `max` arguments.'));
+
+$this->isTrue(weeBigNumberValidator::test('42', array('min' => '41', 'max' => '43')),
+	_('weeBigNumberValidator should return true if the value is in the range of the `min` and `max` arguments.'));
+
+// Objects
+
+$this->isTrue(weeBigNumberValidator::test(new PrintableInput_testBigNumberValidator),
+	_('weeBigNumberValidator::test should return true when the value is an instance of Printable which returns a valid number.'));
+
+$this->isTrue(weeBigNumberValidator::test(new CastableInput_testBigNumberValidator),
+	_('weeBigNumberValidator::test should return true when the value is an object castable to string which casts to a valid number.'));
---
+++ wee/validators/weeBigNumberValidator.class.php
@@ -0,0 +1,241 @@
+<?php
+
+/*
+	Web:Extend
+	Copyright (c) 2008 Dev:Extend
+
+	This library is free software; you can redistribute it and/or
+	modify it under the terms of the GNU Lesser General Public
+	License as published by the Free Software Foundation; either
+	version 2.1 of the License, or (at your option) any later version.
+
+	This library is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+	Lesser General Public License for more details.
+
+	You should have received a copy of the GNU Lesser General Public
+	License along with this library; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+if (!defined('ALLOW_INCLUSION')) die;
+
+/**
+	A big number validator.
+
+	Otherwise the comparisons for the `min` and `max` parameters will be performed using
+	string functions.
+
+	This validator accepts the following arguments:
+	 - format:			The format of the number to validate, can be either 'int' or 'float', defaults to 'int'.
+	 - int_error:		The error message used if not a valid integer representation and the requested `format` is 'int'.
+	 - max:				The upper bound of the range of the valid numbers.
+	 - max_error:		The error message used if the number is greater than the `max` argument.
+	 - min:				The lower bound of the range of the valid numbers.
+	 - min_error:		The error message used if the number is smaller than the `min` argument.
+	 - invalid_error:	The error message used if the input is not numeric.
+*/
+
+class weeBigNumberValidator extends weeValidator
+{
+	/**
+		Default error messages.
+	*/
+
+	protected $aErrors = array(
+		'int'		=> 'Input must be an integer value.',
+		'max'		=> 'Input must not be greater than %max%.',
+		'min'		=> 'Input must not be smaller than %min%.',
+		'invalid'	=> 'Input must be a number.',
+	);
+
+	/**
+		The arguments of the validator.
+	*/
+
+	protected $aArgs = array(
+		'format' => 'int'
+	);
+
+	/**
+		Initialises a new number validator.
+
+		$mValue must be either a string, an instance of Printable or an object castable to string.
+
+		@param	$mValue						The value to validate.
+		@param	$aArgs						The configuration arguments of the validator.
+		@throw	DomainException				$mValue is not of a correct type.
+		@throw	DomainException				The `format` argument is invalid.
+		@throw	DomainException				The `max` argument is invalid.
+		@throw	DomainException				The `min` argument is invalid.
+		@throw	InvalidArgumentException	The `min` and `max` arguments do not form a valid number range.
+	*/
+
+	public function __construct($mValue, array $aArgs = array())
+	{
+		!isset($aArgs['format']) or $aArgs['format'] == 'int' or $aArgs['format'] == 'float'
+			or burn('InvalidArgumentException',
+				_('The `format` argument must be either "int" or "float".'));
+
+		if (is_object($mValue))
+		{
+			if ($mValue instanceof Printable)
+				$mValue = $mValue->toString();
+			elseif (method_exists($mValue, '__toString'))
+				$mValue = (string)$mValue;
+		}
+
+		is_string($mValue)
+			or burn('DomainException',
+				_('$mValue is not of a correct type.'));
+
+		!isset($aArgs['min']) or is_string($aArgs['min']) and $this->isValidInput($aArgs['min'])
+			or burn('DomainException',
+				_('The `min` argument is invalid.'));
+
+		!isset($aArgs['max']) or is_string($aArgs['max']) and $this->isValidInput($aArgs['max'])
+			or burn('DomainException',
+				_('The `max` argument is invalid.'));
+
+		if (isset($aArgs['min'], $aArgs['max']))
+		{
+			$this->compare($aArgs['min'], $aArgs['max']) < 0
+				or burn('InvalidArgumentException',
+					_('The `min` and `max` arguments do not form a valid number range.'));
+		}
+
+		parent::__construct($mValue, $aArgs);
+	}
+
+	/**
+		Compares two big numbers.
+
+		Returns an integer negative if $mLeft is less than $mRight, positive if $mLeft is greater
+		than $mRight and 0 if $mLeft and $mRight are equal.
+
+		@param	$sLeft						The left value.
+		@param	$sRight						The right value.
+		@return	int							The result of the comparison.
+	*/
+
+	protected function compare($sLeft, $sRight)
+	{
+		$iLeftDecPoint	= strpos($sLeft, '.');
+		$iRightDecPoint	= strpos($sRight, '.');
+		if ($iLeftDecPoint !== false || $iRightDecPoint !== false)
+		{
+			$sLeftDec	= '0';
+			$sRightDec	= '0';
+
+			if ($iLeftDecPoint !== false)
+			{
+				$sLeftDec .= rtrim(substr($sLeft, $iLeftDecPoint + 1), '0');
+				$sLeft = substr($sLeft, 0, $iLeftDecPoint);
+			}
+
+			if ($iRightDecPoint !== false)
+			{
+				$sRightDec .= rtrim(substr($sRight, $iRightDecPoint + 1), '0');
+				$sRight = substr($sRight, 0, $iRightDecPoint);
+			}
+		}
+
+		$bLeftIsNeg		= $sLeft[0] == '-';
+		$bRightIsNeg	= $sRight[0] == '-';
+
+		if ($bLeftIsNeg != $bRightIsNeg)
+			return $bLeftIsNeg ? -1 : 1;
+
+		if ($bLeftIsNeg)
+		{
+			$sLeft	= substr($sLeft, 1);
+			$sRight	= substr($sRight, 1);
+		}
+
+		$sLeft	= ltrim($sLeft, '0');
+		$sRight	= ltrim($sRight, '0');
+		$iLeft	= strlen($sLeft);
+		$iRight	= strlen($sRight);
+
+		if ($iLeft != $iRight)
+			return $bLeftIsNeg ? $iRight - $iLeft : $iLeft - $iRight;
+
+		$i = $bLeftIsNeg ? strcmp($sRight, $sLeft) : strcmp($sLeft, $sRight);
+		if ($i != 0 || !isset($sLeftDec))
+			return $i;
+
+		return $bLeftIsNeg ? strcmp($sRightDec, $sLeftDec) : strcmp($sLeftDec, $sRightDec);
+	}
+
+	/**
+		Returns whether a given input is a valid number.
+
+		@param	$sInput						The input.
+		@return	bool						Whether the given input is a valid number.
+	*/
+
+	protected function isValidInput($sInput)
+	{
+		$i = strlen($sInput);
+		if (!$i)
+			return false;
+
+		$bIsNeg = $sInput[0] == '-';
+		if ($bIsNeg)
+		{
+			$sInput = substr($sInput, 1);
+			if (!--$i)
+				return false;
+		}
+
+		$iDecPoint = strpos($sInput, '.');
+		if ($iDecPoint === false)
+		{
+			if ($bIsNeg && !trim($sInput, '0'))
+				return false;
+
+			return ctype_digit($sInput);
+		}
+
+		if (!$iDecPoint || $iDecPoint == $i - 1 || strpos($sInput, '.', $iDecPoint + 1))
+			return false;
+
+		if ($bIsNeg && trim($sInput, '0') == '.')
+			return false;
+
+		return ctype_digit(substr($sInput, 0, $iDecPoint)) && ctype_digit(substr($sInput, $iDecPoint + 1));
+	}
+
+	/**
+		Convenience function for inline validating of variables.
+
+		@param	$mValue						The value to validate.
+		@param	$aArgs						The configuration arguments of the validator.
+		@return	bool						Whether the variable is valid.
+	*/
+
+	public static function test($mValue, array $aArgs = array())
+	{
+		$o = new self($mValue, $aArgs);
+		return !$o->hasError();
+	}
+
+	/**
+		Validates a number.
+	*/
+
+	protected function validate()
+	{
+		if (!$this->isValidInput($this->mValue))
+			return $this->setError('invalid');
+
+		if ($this->aArgs['format'] == 'int' && strpos($this->mValue, '.') !== false)
+			return $this->setError('int');
+
+		if (isset($this->aArgs['max']) && $this->compare($this->mValue, $this->aArgs['max']) > 0)
+			$this->setError('max');
+		elseif (isset($this->aArgs['min']) && $this->compare($this->mValue, $this->aArgs['min']) < 0)
+			$this->setError('min');
+	}
+}
