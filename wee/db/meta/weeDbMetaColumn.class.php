e0695b98dc9131096cebab1ff5477e1c990b75a6
nox <nox@dev-extend.eu>
2008-11-24 00:23:25 +0000

    -ugly_and_outdated_code

--- wee/db/meta/weeDbMetaColumn.class.php
+++ wee/db/meta/weeDbMetaColumn.class.php
@@ -30,7 +30,7 @@ abstract class weeDbMetaColumn extends weeDbMetaTableObject
 	/**
 		Returns the default value of the column.
 
-		@return	string	The default value of the column.
+		@return	string			The default value of the column.
 	*/
 
 	abstract public function defaultValue();
@@ -38,35 +38,15 @@ abstract class weeDbMetaColumn extends weeDbMetaTableObject
 	/**
 		Returns a validator for the column.
 
-		@return	weeValidator			A validator appropriate for the column.
-		@throw	UnhandledTypeException	The type of the column is not handled by dbmeta.
+		@return	weeValidator	A validator appropriate for the column.
 	*/
 
-	public function getValidator()
-	{
-		switch ($this->aData['type'])
-		{
-			case 'tinyint':
-			case 'smallint':
-			case 'mediumint':
-			case 'int':
-			case 'bigint':
-				return weeDbMeta::getIntegerValidator($this->aData['type']);
-
-			case 'char':
-			case 'varchar':
-				return new weeStringValidator(array('max' => $this->aData['max_length']));
-
-			default:
-				burn('UnhandledTypeException',
-					sprintf(_WT('Type "%s" is not handled by dbmeta.'), $this->aData['type']));
-		}
-	}
+	abstract public function getValidator();
 
 	/**
 		Returns whether the column has a default value.
 
-		@return	bool	Whether the column has a default value.
+		@return	bool			Whether the column has a default value.
 	*/
 
 	abstract public function hasDefault();
@@ -74,7 +54,7 @@ abstract class weeDbMetaColumn extends weeDbMetaTableObject
 	/**
 		Returns whether the column can contain null values.
 	
-		@return	bool	Whether the column can contain null values.
+		@return	bool			Whether the column can contain null values.
 	*/
 
 	abstract public function isNullable();
@@ -82,7 +62,7 @@ abstract class weeDbMetaColumn extends weeDbMetaTableObject
 	/**
 		Returns the number of the column in the table.
 
-		@return	int		The number of the column in the table.
+		@return	int				The number of the column in the table.
 	*/
 
 	public function num()
