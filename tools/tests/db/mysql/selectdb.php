4f3bd669abccf61a7f4872cf22ab93f49a66b9b9
nox <nox@dev-extend.eu>
2008-12-29 18:51:15 +0000

    Add tests for weeMySQL(i)Database::selectDb.

---
+++ tools/tests/db/mysql/selectdb.php
@@ -0,0 +1,14 @@
+<?php
+
+require(dirname(__FILE__) . '/connect.php.inc');
+
+try {
+	$oDb->selectDb('database_which_does_not_exist');
+	$this->fail(_WT('weeMySQLDatabase::selectDb should throw a DatabaseException when the given database does not exist.'));
+} catch (DatabaseException $e) {}
+
+try {
+	$oDb->selectDb($oDb->queryValue('SELECT DATABASE()'));
+} catch (DatabaseException $e) {
+	$this->fail(_WT('weeMySQLDatabase::selectDb should not throw a DatabaseException when the given database can be selected.'));
+}
