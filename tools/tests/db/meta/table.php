cd85cbd15d7904cbbabbcdf5b06080c05328f9fc
nox <nox@dev-extend.eu>
2009-01-26 03:15:22 +0000

    Update tests per 'fd0ad51d5143a2587e1b0252970e5f920c807ec6'

--- tools/tests/db/meta/table.php
+++ tools/tests/db/meta/table.php
@@ -19,11 +19,6 @@ class weeDbMetaTable_test extends weeDbMetaTable {
 $o = new weeDbMetaTable_test;
 
 try {
-	$o->instantiateObject('class_which_does_not_exist', array());
-	$this->fail(_WT('weeDbMetaTable::instantiateObject should throw an InvalidArgumentException when the given class does not exist.'));
-} catch (InvalidArgumentException $e) {}
-
-try {
 	$o->instantiateObject('stdClass', array());
 	$this->fail(_WT('weeDbMetaTable::instantiateObject should throw an InvalidArgumentException when the given class is not a subclass of weeDbMetaTableObject.'));
 } catch (InvalidArgumentException $e) {}
