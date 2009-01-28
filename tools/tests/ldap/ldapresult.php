a8b6ced037b57fce1f08249ed7c1f4d0a439ea6d
yl <yl@dev-extend.eu>
2009-01-28 13:33:49 +0000

    Modified tests for wee/ldap/weeLDAPResult.class.php

--- tools/tests/ldap/ldapresult.php
+++ tools/tests/ldap/ldapresult.php
@@ -7,38 +7,21 @@ try {
 
 	$sDN = 'dc=example,dc=com';
 	$oResult = $o->search($sDN, 'ou=*');
-	$i = $oResult->numResults();
 
-	$this->isEqual(2, $i,
-		sprintf(_WT('weeLDAPResult::numResults should return 2 got %s instead.'), $i));
+	$oEntry = $oResult->fetch();
+	$this->isEqual($oEntry->getDN(), 'ou=customers,dc=example,dc=com',
+		_WT('weeLDAPResult::fetch did not get the expected entry.'));
 
-	$sDN = 'ou=customers,dc=example,dc=com';
-	$this->isEqual($sDN, $oResult->current()->getDN(),
-		sprintf(_WT('weeLDAPResult::current should return an weeLDAPEntry object with DN: %s'), $sDN));
+	$this->isEqual(2, $oResult->numResults(),
+		_WT('weeLDAPResult::numResults did not get the expected number of entries.'));
 
-	$oResult->fetch();
-	$this->isEqual($sDN, $oResult->current()->getDN(),
-		sprintf(_WT('weeLDAPResult::fetch : weeLDAPResult::current should return an weeLDAPEntry object with DN: %s. '), $sDN));
-	
 	$aEntries = $oResult->fetchAll();
-	$this->isEqual(2, $aEntries[0]['count'], // customers & countries
-		sprintf(_WT('weeLDAPResult::numResults should return 2 elements got %s instead.'), $aEntries[0]['count']));
+	$this->isEqual(2, $aEntries['count'], // customers & countries
+		sprintf(_WT('weeLDAPResult::fetchAll did not get the expected number of entries.'), $aEntries['count']));
 
 	$oResult->sort('ou');
-	$oResult->fetch();
-	$sDN = 'ou=countries,dc=example,dc=com';
-	$this->isEqual($sDN, $oResult->current()->getDN(),
-		sprintf(_WT('weeLDAPResult::sort : weeLDAPResult::current should return an weeLDAPEntry object with DN: %s.'), $sDN));
-
-	$oResult->next();
-	$this->isEqual(1, $oResult->key(),
-		sprintf(_WT('weeLDAPResult::key should return 1 got %s instead.'), $oResult->key()));
-
-	$this->isTrue($oResult->valid(),
-		sprintf(_WT('weeLDAPResult::valid should return true got %s instead.'), $oResult->valid()));
-
-	$this->isFalse($oResult->offsetExists(-1),
-		_WT('weeLDAPResult::offSetExists should not find this offset.'));
+	$this->isEqual('ou=countries,dc=example,dc=com', $oResult->fetch()->getDN(),
+		_WT('weeLDAPResult::sort did not sort the entries.'));
 
 } catch (LDAPException $e) {
 	$this->fail(_WT('weeLDAP should not throw an LDAPException.'));
