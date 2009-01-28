1cf40953861e1f1a40786f8be9f6cefc8a978a9a
yl <yl@dev-extend.eu>
2009-01-28 15:44:26 +0000

    Added tests for wee/ldap/weeLDAP.class.php

--- tools/tests/ldap/ldap.php
+++ tools/tests/ldap/ldap.php
@@ -5,43 +5,36 @@ function_exists('ldap_connect') or $this->skip();
 try {
 	require(ROOT_PATH . 'tools/tests/ldap/init.php.inc');
 
-	$sDN = 'dc=example, dc=com';
-	$this->isEqual(1, $o->search($sDN, 'c=FR')->numResults(),
-		sprintf(_WT('weeLDAP::search should find : c=US, ou=countries, dc=example, dc=com in the DN : %s.'), $sDN));
-
 	$sDN = 'cn=Anakin Skywalker, ou=customers, dc=example, dc=com';
 	$sAttr = 'objectClass';
 	$sValue = 'person';
 	$this->isTrue($o->compare($sDN, $sAttr, $sValue),
-		sprintf(_WT('weeLDAP::compare should return true because the %s attribute of the DN %s is %s.'), $sAttr, $sDN, $sValue));
+		sprintf(_WT('weeLDAP::compare should return true because the "%s" attribute of the DN "%s" is "%s".'), $sAttr, $sDN, $sValue));
 
 	$sDN = 'dc=example, dc=com';
-	$a = $o->ls($sDN, 'ou=*')->fetchAll();
-	$this->isEqual(2, $a['count'], //customers & countries
-		sprintf(_WT('weeLDAP::ls should find 2 Organizational Units (ou), in the DN : %s.'), $sDN));
+	$iResult = $o->ls($sDN, 'ou=*')->numResults();
+	$this->isEqual(2, $iResult,
+		sprintf(_WT('weeLDAP::ls did not get the expected result in the DN : "%s".'), $sDN));
 
-	$oEntries = $o->search($sDN, 'ou=*')->fetchAll();
-	$this->isEqual(2, $oEntries['count'], //customers & countries
-		sprintf(_WT('weeLDAP::search should find 2 Organizational Units (ou), in the DN : %s.'), $sDN));
-
-	$sDN = 'cn=Anakin Skywalker, ou=customers, dc=example, dc=com';
-	$o->modify($sDN, array(
+	$o->modify('cn=Anakin Skywalker, ou=customers, dc=example, dc=com', array(
 		'telephonenumber' 	=> '5555-6666',
 		));
 
-	$sDNTmp = 'ou=customers, dc=example, dc=com';
-	$oEntry = $o->ls($sDNTmp, 'cn=Anakin Skywalker')->fetch();
-
+	$oEntry = $o->ls('ou=customers, dc=example, dc=com', 'cn=Anakin Skywalker')->fetch();
 	$aAttr = $oEntry->getAttributes();
-
+	
 	$this->isEqual('5555-6666', $aAttr['telephoneNumber'][0],
-		sprintf(_WT('weeLDAP::modify should modify the telephonenumber attribute of %s.'), $sDN));
+		_WT('weeLDAP::modify should did not modify the telephonenumber attribute.'));
 
-	$sDN = 'c=US, ou=countries, dc=example, dc=com';
+	$sDN = 'ou=countries, dc=example, dc=com';
+	$aEntries = $o->read($sDN)->fetchAll();
 
-	$aEntries = $o->read($sDN, 'objectClass=*')->fetchAll(); //TODO pk 'locality' 'ou' 'c' throw LDAPException && pk numResults retourn 1
-	$this->isEqual(2, $oEntries[0]['count'], //FR & US
-		sprintf(_WT('weeLDAP::read should read 2 entries, in the DN : %s.'), $sDN));
+	$this->isEqual(2, $aEntries[0]['count'], //FR & US
+		sprintf(_WT('weeLDAP::read should find entries, in the DN : "%s".'), $sDN));
+
+	$sDN = 'dc=example, dc=com';
+	$this->isEqual(2, $o->search($sDN, 'ou=*')->numResults(),
+		sprintf(_WT('weeLDAP::search did not find the expected result in the DN : "%s".'), $sDN));
 
 } catch (LDAPException $e) {
 	$this->fail(_WT('weeLDAP should not throw an LDAPException.'));
