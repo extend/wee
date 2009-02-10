a9d8d88a74f0e772cee811d04718a9c31d3e9edb
essen <essen@dev-extend.eu>
2009-02-10 16:11:50 +0000

    Missing changes from merge. I knew I did it wrong.

--- tools/tests/app/taconite/applyto.php
+++ tools/tests/app/taconite/applyto.php
@@ -1,55 +1,60 @@
 <?php
 
+function weeTaconite_create()
+{
+	return new weeTaconite;
+}
+
 // after
 
-$o = weeTaconite::create()->addTag('after', 'this', '<that/>');
+$o = weeTaconite_create()->addTag('after', 'this', '<that/>');
 
 $this->isEqual('<root><this/><that/></root>', $o->applyTo('<root><this/></root>'),
 	_WT('weeTaconite does not correctly apply `after` tags.'));
 
 // append
 
-$o = weeTaconite::create()->addTag('append', 'this', '<append/>');
+$o = weeTaconite_create()->addTag('append', 'this', '<append/>');
 
 $this->isEqual('<root><this><that/><append/></this></root>', $o->applyTo('<root><this><that/></this></root>'),
 	_WT('weeTaconite does not correctly apply `append` tags.'));
 
 // before
 
-$o = weeTaconite::create()->addTag('before', 'this', '<that/>');
+$o = weeTaconite_create()->addTag('before', 'this', '<that/>');
 
 $this->isEqual('<root><that/><this/></root>', $o->applyTo('<root><this/></root>'),
 	_WT('weeTaconite does not correctly apply `before` tags.'));
 
 // prepend
 
-$o = weeTaconite::create()->addTag('prepend', 'this', '<prepend/>');
+$o = weeTaconite_create()->addTag('prepend', 'this', '<prepend/>');
 
 $this->isEqual('<root><this><prepend/><that/></this></root>', $o->applyTo('<root><this><that/></this></root>'),
 	_WT('weeTaconite does not correctly apply `prepend` tags.'));
 
 // remove
 
-$o = weeTaconite::create()->addTag('remove', 'this');
+$o = weeTaconite_create()->addTag('remove', 'this');
 
 $this->isEqual('<root/>', $o->applyTo('<root><this/></root>'),
 	_WT('weeTaconite does not correctly apply `remove` tags.'));
 
 // replace
 
-$o = weeTaconite::create()->addTag('replace', 'this', '<that/>');
+$o = weeTaconite_create()->addTag('replace', 'this', '<that/>');
 
 $this->isEqual('<root><that/></root>', $o->applyTo('<root><this/></root>'),
 	_WT('weeTaconite does not correctly apply `replace` tags.'));
 
 // replaceContent
 
-$o = weeTaconite::create()->addTag('replaceContent', 'this', '<replaceContent/>');
+$o = weeTaconite_create()->addTag('replaceContent', 'this', '<replaceContent/>');
 
 $this->isEqual('<root><this><replaceContent/></this></root>', $o->applyTo('<root><this><that/></this></root>'),
 	_WT('weeTaconite does not correctly apply `replaceContent` tags.'));
 
-$o = weeTaconite::create()->addTag('invalid', '', '<invalidXML>');
+$o = weeTaconite_create()->addTag('invalid', '', '<invalidXML>');
 
 // Bad Taconite
 
@@ -61,6 +66,6 @@ try {
 // Bad document
 
 try {
-	weeTaconite::create()->applyTo('<root>');
+	weeTaconite_create()->applyTo('<root>');
 	$this->fail(_WT('weeTaconite::applyTo should throw a BadXMLException when the given string is not a well-formed XML document.'));
 } catch (BadXMLException $e) {}
