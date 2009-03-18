57dd72cdbddccc0c3e9728433662179a51421a66
nox <nox@dev-extend.eu>
2009-03-18 00:38:54 +0000

    Merge selectdb test.

--- tools/tests/db/mysql/selectdb.php
+++ tools/tests/db/mysql/selectdb.php
@@ -1,14 +1,6 @@
 <?php
 
-require(dirname(__FILE__) . '/connect.php.inc');
-
-try {
-	$oDb->selectDb('database_which_does_not_exist');
-	$this->fail(_WT('weeMySQLDatabase::selectDb should throw a DatabaseException when the given database does not exist.'));
-} catch (DatabaseException $e) {}
-
-try {
-	$oDb->selectDb($oDb->queryValue('SELECT DATABASE()'));
-} catch (DatabaseException $e) {
-	$this->fail(_WT('weeMySQLDatabase::selectDb should not throw a DatabaseException when the given database can be selected.'));
-}
+if (!isset($oDb))
+	require('connect.php.inc');
+$sCurrentDb = $oDb->queryValue('SELECT DATABASE()');
+require(dirname(__FILE__) . '/../selectdb.php.inc');
--- tools/tests/db/mysqli/selectdb.php
+++ tools/tests/db/mysqli/selectdb.php
@@ -1,14 +1,4 @@
 <?php
 
-require(dirname(__FILE__) . '/connect.php.inc');
-
-try {
-	$oDb->selectDb('database_which_does_not_exist');
-	$this->fail(_WT('weeMySQLiDatabase::selectDb should throw a DatabaseException when the given database does not exist.'));
-} catch (DatabaseException $e) {}
-
-try {
-	$oDb->selectDb($oDb->queryValue('SELECT DATABASE()'));
-} catch (DatabaseException $e) {
-	$this->fail(_WT('weeMySQLiDatabase::selectDb should not throw a DatabaseException when the given database can be selected.'));
-}
+require('connect.php.inc');
+require(dirname(__FILE__) . '/../mysql/selectdb.php');
